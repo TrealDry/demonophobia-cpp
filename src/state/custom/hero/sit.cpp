b356e9e426375afb914aa1d0533c0f79859c4835
TrealDry <81185574+TrealDry@users.noreply.github.com>
2025-06-19 08:10:22 +0300

    anim hotfix

--- src/state/custom/hero/sit.cpp
+++ src/state/custom/hero/sit.cpp
@@ -8,12 +8,12 @@ void SitState::enter() {
 
 void SitState::exit() {
     m_owner->m_animations[ANIM_SIT].stop();
-    m_owner->changeLook(IsKeyPressed(KEY_D));
 }
 
 void SitState::update() {
     if (IsKeyPressed(KEY_A) || IsKeyPressed(KEY_D)) {
         m_owner->changeState(m_owner->m_crawlState);
+        m_owner->changeLook(IsKeyPressed(KEY_D));
     } else if (!IsKeyDown(KEY_S) && (!IsKeyDown(KEY_A) || !IsKeyDown(KEY_D))) {
         m_owner->changeState(m_owner->m_idleState);
     }
